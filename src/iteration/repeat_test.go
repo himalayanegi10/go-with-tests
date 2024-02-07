60d47d2acfa994c5c0a245436b7bb61adb685b6a
himalayanegi10 <himalayanegi10@gmail.com>
2024-02-07 20:07:39 +0530

    Adding more TCs

--- src/iteration/repeat_test.go
+++ src/iteration/repeat_test.go
@@ -6,8 +6,21 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("Repeat 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectMatch(t, repeated, expected)
+	})
+
+	t.Run("Repeat 10 times", func(t *testing.T) {
+		repeated := Repeat("a", 10)
+		expected := "aaaaaaaaaa"
+		assertCorrectMatch(t, repeated, expected)
+	})
+}
+
+func assertCorrectMatch(t *testing.T, repeated, expected string) {
+	t.Helper()
 	if repeated != expected {
 		t.Errorf("Expected %q found %q", expected, repeated)
 	}
@@ -16,12 +29,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0 ; i < b.N ; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
 
 func ExampleRepeat() {
-	result := Repeat("b")
+	result := Repeat("b", 5)
 	fmt.Println(result)
 	// Output: bbbbb
 }
