9e28c0b2080cbf4ef5ca2ba8c6feaca0089ba718
himalayanegi10 <himalayanegi10@gmail.com>
2024-02-15 23:17:07 +0530

    refactor

--- src/pointersanderrors/pointers.go
+++ src/pointersanderrors/pointers.go
@@ -1,13 +1,25 @@
 package pointersanderrors
 
+import "fmt"
+
+type Bitcoin float64
+
 type Wallet struct {
-	balance float64
+	balance Bitcoin
 }
 
-func (w *Wallet) Balance() float64 { // use pointers just like C
+type Stringer interface {
+	String() string
+}
+
+func (w *Wallet) Balance() Bitcoin { // use pointers just like C
 	return w.balance
 }
 
-func (w *Wallet) Deposit(amount float64) { // use pointers just like C
+func (w *Wallet) Deposit(amount Bitcoin) { // use pointers just like C
 	w.balance += amount
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%g BTC", b)
 }
--- src/pointersanderrors/pointers_test.go
+++ src/pointersanderrors/pointers_test.go
@@ -7,14 +7,14 @@ import (
 func TestPointers(t *testing.T) {
 	t.Run("Bitcoin Wallet", func(t *testing.T) {
 		wallet := Wallet{}
-		wallet.Deposit(10.0)
+		wallet.Deposit(Bitcoin(10.0))
 		
 		got := wallet.Balance()
 		
-		want := 10.0
+		want := Bitcoin(10.0)
 
 		if got != want {
-			t.Errorf("Wanted %g got %g", want, got)
+			t.Errorf("Wanted %s got %s", want, got)
 		}
 	})
 }
