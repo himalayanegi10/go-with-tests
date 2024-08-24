1476710bb69d3734a65a8e5eb6e6f4fcdbdd0680
himalayanegi10 <himalayanegi10@gmail.com>
2024-08-24 16:20:24 +0530

    refactoring tests

--- src/mocking/mock_test.go
+++ src/mocking/mock_test.go
@@ -6,27 +6,27 @@ import (
 	"testing"
 )
 
-func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	SpySleeper := &SpySleeper{}
-	Countdown(buffer, SpySleeper)
+func TestCountDown(t *testing.T) {
 
-	got := buffer.String()
-	want := `3
+	t.Run("print 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		SpySleeper := &SpySleeper{}
+		Countdown(buffer, SpySleeper)
+
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	if SpySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", SpySleeper.Calls)
-	}
-}
-
-func TestCountDown(t *testing.T) {
+		if SpySleeper.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", SpySleeper.Calls)
+		}			
+	})
 
 	t.Run("sleep before every print", func(t *testing.T) {
 		spySleepPrinter := &SpyCountdownOperations{}
