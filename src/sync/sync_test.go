cb017088799d064eca188dda5f3a17d6e43eaacc
himalayanegi10 <himalayanegi10@gmail.com>
2024-09-22 20:47:41 +0530

    handling mutex copy issue using pointers

--- src/sync/sync_test.go
+++ src/sync/sync_test.go
@@ -7,7 +7,8 @@ import (
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leave it at 3", func(t *testing.T) {
-		counter := Counter{}
+		// counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
@@ -17,7 +18,8 @@ func TestCounter(t *testing.T) {
 
 	t.Run("it runs safely concurrently", func(t *testing.T) {
 		wantedCount := 1000
-		counter := Counter{}
+		// counter := Counter{}
+		counter := NewCounter()
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCount)
@@ -34,7 +36,7 @@ func TestCounter(t *testing.T) {
 	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
