6f1b19c6b0d299c090e3349fc874fba5f1225041
himalayanegi10 <himalayanegi10@gmail.com>
2024-10-08 10:27:22 +0530

    refactor

--- src/propertybasedtest/pbt.go
+++ src/propertybasedtest/pbt.go
@@ -5,17 +5,19 @@ import "strings"
 func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
-	for i := arabic; i > 0; i-- {
-		if i == 5 {
-			result.WriteString("V")
-			break
-		}
-		if i == 4 {
-			result.WriteString("IV")
-			break
-		}
-		result.WriteString("I")
-	}
 
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
+	}
 	return result.String()
 }
