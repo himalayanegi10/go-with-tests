c8c44144ce0108dcb1bb5d659e3891ef4de3698f
himalayanegi10 <himalayanegi10@gmail.com>
2024-10-26 13:43:23 +0530

    Stack Data stucture without generics

--- src/generics/generics_test.go
+++ src/generics/generics_test.go
@@ -16,6 +16,46 @@ func TestAssertFunctions(t *testing.T) {
 	})
 }
 
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+
+		// check stack is empty
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := new(StackOfStrings)
+
+		// check stack is empty
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfStrings.Push("hello")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfStrings.Push("himalaya")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "himalaya")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "hello")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+}
+
 func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
@@ -27,5 +67,20 @@ func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
 		t.Errorf("didn't want %v", got)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
 	}
 }
