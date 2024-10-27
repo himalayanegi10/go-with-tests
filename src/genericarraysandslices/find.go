65e9090b562e9abfe01281d61401cdc8e8d2883d
himalayanegi10 <himalayanegi10@gmail.com>
2024-10-28 00:59:36 +0530

    Find using Reduce

---
+++ src/genericarraysandslices/find.go
@@ -0,0 +1,11 @@
+package main
+
+
+func Find[T any](iterable []T, got func(T) bool) (value T, found bool) {
+	for _, item := range iterable {
+		if (got(item)) {
+			return item, true
+		}
+	}
+	return 
+}
