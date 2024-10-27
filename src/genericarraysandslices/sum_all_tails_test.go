277f7cf750592d61dfa5a41922c6afb58d5a6278
himalayanegi10 <himalayanegi10@gmail.com>
2024-10-27 22:08:34 +0530

    Multiplication/concatenation using reduce

--- src/genericarraysandslices/sum_all_tails_test.go
+++ src/genericarraysandslices/sum_all_tails_test.go
@@ -43,4 +43,28 @@ func TestReduce(t *testing.T) {
 			t.Errorf("got %v, expected %v", got, want)
 		}
 	})
+
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1,2,3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"Hello", "World"}, concatenate, ""), "HelloWorld")
+		
+	})
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
