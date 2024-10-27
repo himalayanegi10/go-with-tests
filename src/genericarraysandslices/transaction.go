60946694c361c311c72a9762c876e81563f2c39a
himalayanegi10 <himalayanegi10@gmail.com>
2024-10-28 00:37:44 +0530

    verbose transaction code using Reduce

--- src/genericarraysandslices/transaction.go
+++ src/genericarraysandslices/transaction.go
@@ -1,9 +1,16 @@
 package main
 
-type Transaction struct {
-	From, To string
-	Sum float64
-}
+type (
+	Transaction struct {
+		From, To string
+		Sum float64
+	}
+
+	Account struct {
+		Name	string
+		Balance	float64
+	}
+)
 
 func BalanceFor(transactions []Transaction, name string) float64 {
 	adjustBalance := func(currentBalance float64, t Transaction) float64 {
@@ -15,4 +22,25 @@ func BalanceFor(transactions []Transaction, name string) float64 {
 		return currentBalance
 	}
 	return Reduce(transactions, adjustBalance, 0.0)
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,	
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	} else if (transaction.To == a.Name) {
+		a.Balance += transaction.Sum
+	}
+	return a
+}
+
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
 }
--- src/genericarraysandslices/transaction_test.go
+++ src/genericarraysandslices/transaction_test.go
@@ -22,3 +22,24 @@ func TestBadBank(t *testing.T) {
 	AssertEqual(t, BalanceFor(transactions, "A"), -75)
 	AssertEqual(t, BalanceFor(transactions, "C"), -25)
 }
+
+func TestNewBadBank(t *testing.T) {
+	var (
+		himalaya = Account{Name: "Himalaya", Balance: 100}
+		mahendra = Account{Name: "Mahendra", Balance: 75}
+		tulsi = Account{Name: "Tulsi", Balance: 200}
+
+		transactions = []Transaction{
+			NewTransaction(himalaya, mahendra, 100),
+			NewTransaction(himalaya, tulsi, 500),
+		}
+	)
+
+	newBalanceFor := func(account Account) float64 {
+		return NewBalanceFor(account, transactions).Balance
+	}
+
+	AssertEqual(t, newBalanceFor(himalaya), -500)
+	AssertEqual(t, newBalanceFor(mahendra), 175)
+	AssertEqual(t, newBalanceFor(tulsi), 700)
+}
